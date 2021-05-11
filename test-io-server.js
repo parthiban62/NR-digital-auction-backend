30dbb9cac202c127ab673d5db1e56a72f68d062b
parthiban <parthibanjayaraj@gmail.com>
2021-05-11 19:10:15 +0530

    Updated server js file for socket io

--- test-io-server.js
+++ test-io-server.js
@@ -1,19 +1,19 @@
 // test-io-server.js
-const express = require('express');
-var cors = require('cors')
+const express = require("express");
+var cors = require("cors");
 const app = express();
-app.use(cors())
+app.use(cors());
 const PORT = process.env.PORT || 2000;
 
-const bodyParser = require('body-parser');
-const http = require('http').Server(app);
-const socket = require('socket.io')(http);
+const bodyParser = require("body-parser");
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
-app.set('port', PORT);
+app.set("port", PORT);
 
 // Express Middleware for serving static
 // files and parsing the request body
-app.use(express.static('public'));
+app.use(express.static("public"));
 // create application/x-www-form-urlencoded parser
 app.use(bodyParser.urlencoded({ extended: true }));
 // create application/json parser
@@ -21,37 +21,33 @@ app.use(bodyParser.json());
 
 // Create the socket server
 //Controllers
-var auctionController = require('./Controller/AuctionController')();
+var auctionController = require("./Controller/AuctionController")();
 app.use("/api/auctions", auctionController);
 
-var biddingController = require('./Controller/BiddingController')();
+var biddingController = require("./Controller/BiddingController")();
 app.use("/api/bidding", biddingController);
 
-var settingsController = require('./Controller/SettingsController')();
-app.use('/api/settings', settingsController);
+var settingsController = require("./Controller/SettingsController")();
+app.use("/api/settings", settingsController);
 
-//var socket = require('socket.io')(PORT);
-socket.on('connection', function(client) {
+//var io = require('io.io')(PORT);
+io.on("connection", function (socket) {
+  socket.on("disconnect", onDisconnect);
+  socket.on("send-message", (message) => {
+    io.emit("message", message);
+  });
 
-   client.on('test', onTest);
-    client.on('disconnect', onDisconnect);
-
-    // Handle a test event from the client
-    function onTest(data) {
-        console.log('Received: "' + data + '" from client: ' + client.id);
-        client.emit('test', "Cheers, " + data);
-    }
-    
-    // Handle a disconnection from the client
-    function onDisconnect() {
-        console.log('Received: disconnect event from client: ' + client.id);
-        client.removeListener('test', onTest);
-        client.removeListener('disconnect', onDisconnect);
-    }
+  // Handle a disconnection from the client
+  function onDisconnect() {
+    console.log("Received: disconnect event from client: " + socket.id);
+    // socket.removeListener('test', onTest);
+    socket.removeListener("disconnect", onDisconnect);
+  }
 });
 
-http.listen(app.get('port'), function () {
-    var datetime = new Date();
-    var message = "Server runnning on Port:- "+ PORT +". Started at :- " + datetime;
-    console.log(message);
+http.listen(app.get("port"), function () {
+  var datetime = new Date();
+  var message =
+    "Server runnning on Port:- " + PORT + ". Started at :- " + datetime;
+  console.log(message);
 });
