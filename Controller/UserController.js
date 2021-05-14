7ee816349e0b2e306a7683e853935112f1172e45
parthiban <parthibanjayaraj@gmail.com>
2021-05-14 16:57:17 +0530

    Enabled cors preflight options handling

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -1,6 +1,8 @@
 const { json } = require('body-parser');
 var express = require('express');
+var cors = require('cors');
 var router = express.Router();
+router.options('*', cors())
 var client = require("../connection/connect")();
 
 //check Redis database connection
